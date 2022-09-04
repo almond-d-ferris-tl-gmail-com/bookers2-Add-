aa07afd12435f689505f178852b467b707e92063
EC2 Default User <ec2-user@ip-172-31-18-106.ap-northeast-1.compute.internal>
2022-09-04 13:06:31 +0000

    [20220904Finish]bookers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
   def index
     @books = Book.all #同じアクション内で同じメソッドは使えない
     @book = Book.new
+    @user = User.find(params[:id])
   end
 
   def show
     #@book = Book.find(params[:id])
-    @books = Book.all #同じアクション内で同じメソッドは使えない
     @book = Book.new
     @user = User.find(params[:id])
+    @books = @user.books #同じアクション内で同じメソッドは使えない
     #@book = @user.book.page(params[:page])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top', as: "top"
   get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:create, :index, :show, :destroy]# :new,
-  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:create, :index, :show, :destroy, :edit]# :new,
+  resources :users, only: [:index, :show, :edit, :update]
 end
